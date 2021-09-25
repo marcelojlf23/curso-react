10d182673a7c903d44dd59afc2343f46444b112d
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-09-25 11:53:47 -0300

    Creating AddTaks e comp button
    Creating AddTaks e comp button

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./AddTask.css";
+
+const AddTask = () => {
+    return ( 
+        <div className="add-task-container">
+            <input type="text" className="add-task-input" /> );
+        </div>
+    )
+};
+ 
+export default AddTask;
