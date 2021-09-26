46b30e4f5747e2e67a1d3f6fce1796188a126ea9
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-09-26 10:18:15 -0300

    adding button details of the task
    adding button details of the task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Task.css';
-import { CgClose } from "react-icons/cg";
+import { CgClose, CgInfo } from "react-icons/cg";
 
 const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
     return ( 
@@ -15,6 +15,9 @@ const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
             <div className="buttons-container">
                 <button className="remove-task-button" onClick={()=>handleTaskDeletion(task.id)}>
                     <CgClose/>
+                </button>
+                <button className="see-task-details" onClick={()=>handleTaskDeletion(task.id)}>
+                    <CgInfo/>
                 </button>
             </div>
         </div> 
